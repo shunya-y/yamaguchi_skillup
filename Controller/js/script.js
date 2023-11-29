9d63dc91ac94bef7b0431c43957f3c9f58bf113b
shunya-y <shunya.yamaguchi@cpara.jp>
2023-11-29 13:19:58 +0900

    by yamaguchi;

---
+++ Controller/js/script.js
@@ -0,0 +1,16 @@
+function drawOptionPartner(doc) {
+    doc.writeln('<OPTION value="送信値１">取引先テスト１');
+    doc.writeln('<OPTION value="送信値２">取引先テスト２');
+}
+function drawOptionBusinessPartnerClassification(doc) {
+    doc.writeln('<OPTION value="送信値１">取引区分１');
+    doc.writeln('<OPTION value="送信値２">取引区分２');
+}
+function drawOptionConsumptionTax(doc) {
+    doc.writeln('<OPTION value="送信値１">消費税率１');
+    doc.writeln('<OPTION value="送信値２">消費税率２');
+}
+function drawOptionProduct(doc) {
+    doc.writeln('<OPTION value="送信値１">商品名１');
+    doc.writeln('<OPTION value="送信値２">商品名２');
+}
