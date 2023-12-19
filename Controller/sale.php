a566bdfee61281de9b4b81eb88d300ed9c5241a0
shunya-y <shunya.yamaguchi@cpara.jp>
2023-12-20 00:17:09 +0900

    by yamaguchi;

--- Controller/sale.php
+++ Controller/sale.php
@@ -1,7 +1,10 @@
 <?php
 //PHPの処理
-//require_once __DIR__.'../Model/model.php';
+require_once __DIR__.'../Model/model.php';
 function listMstClient(){
+
+}
+function listMstProduct(){
 
 }
 ?>
--- Model/model.php
+++ Model/model.php
@@ -1,10 +1,11 @@
 <?php
-    $host = "192.168.11.203";
-    $username = "root";
-    $passwd = "";
-    $dbname = "kenshu_sql";
-    $port = "3306";
-    $socket = "";
+    // 接続情報
+    $host = "192.168.11.203";//DBホスト
+    $username = "root";//DB接続ユーザー
+    $passwd = "";//DB接続パスワード
+    $dbname = "kenshu_sql";//DB名
+    $port = "3306";//ポート名
+    $socket = "";//ソケット名
     // データベースへ接続
     $db = mysqli_connect($host, $username, $passwd, $dbname);
     // 接続チェック
