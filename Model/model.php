ec5f2c92137189613f324c0bc97ab7df211c3ddb
shunya-y <shunya.yamaguchi@cpara.jp>
2023-12-20 23:04:42 +0900

    model修正 by yamaguchi;

--- Model/model.php
+++ Model/model.php
@@ -1,18 +1,21 @@
 <?php
-    // 接続情報
-    $host = "192.168.11.203";//DBホスト
-    $username = "root";//DB接続ユーザー
-    $passwd = "";//DB接続パスワード
-    $dbname = "kenshu_sql";//DB名
-    $port = "3306";//ポート名
-    $socket = "";//ソケット名
-    // データベースへ接続
-    $db = mysqli_connect($host, $username, $passwd, $dbname);
-    // 接続チェック
-    if (!$db) {
-        die('データベースの接続に失敗しました。');
+    $sql = "SELECT * FROM mst_code";
+    try{
+        // 接続情報
+        $host = "192.168.11.203";//DBホスト
+        $username = "root";//DB接続ユーザー
+        $passwd = "OsqeW15P";//DB接続パスワード
+        $dbname = "kenshu_sql";//DB名
+        $port = "3306";//ポート名
+        $option = "charset=utf8";
+        $dsn = "mysql:host=localhost;dbname=" . $host . ";" . $option; #DSN データソースネームmysqlの場合
+        $db = new PDO($dsn, $username, $passwd,//接続するにはまずインスタンスを作成
+            [   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+        echo "データベースの接続に成功しました。 \n";
+        $stmt = $dbh->query($sql);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch(PDOException $e){
+        echo "データベースの接続に失敗しました。 \n";
     }
-    echo "データベースの接続に成功しました！ \n";
-    // データベースの接続を閉じる
-    mysqli_close($db);
 ?>
